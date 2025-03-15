91d779c6a9e45f1fe8cee5bc795a68c5353936b6
unknown <davi.aguiarms@gmail.com>
2025-03-15 00:07:04 -0300

    condicoes

---
+++ conditions.js
@@ -0,0 +1,8 @@
+console.log('o console funcionou corretamente')
+
+var vel = 12
+console.log( `velocidade do seu carro é ${ vel} km/h`)
+if(vel > 60){
+    console.log("voce ultrapassou a velocidade permitida. MULTADO")
+}
+console.log("dirija sempre usando o cinto de seguranca")
